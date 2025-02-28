248d8fa6734fcb85c236b1c1ade046f17366051a
Zaid <zaidoskate@hotmail.com>
2025-02-28 11:47:29 -0600

    Configuracion inicial del proyecto

---
+++ index.js
@@ -0,0 +1,22 @@
+const express = require('express' );
+const app = express();
+const port = 3000;
+
+app.use(express.json());
+
+app.get('/', (req, res) => {
+res.send('Bienvenido a la API');
+
+});
+
+app.get('/saludo/:nombre', (req, res) => {
+    res.send(`Hola, ${req.params.nombre}!`);
+});
+
+app.post('/mensaje', (req, res) => {
+    res.json({ mensaje: `Recibido: ${req.body.mensaje}`});
+});
+
+app.listen(port, () => {
+    console.log(`Servidor escuchando en http://localhost:${port}`);
+});
